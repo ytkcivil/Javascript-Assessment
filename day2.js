f76f163cbe68b9c4811b106c808e517a2363ffec
ytkcivil <97897804+ytkcivil@users.noreply.github.com>
2022-01-26 22:30:31 +0530

    da3assessment

---
+++ day2.js
@@ -0,0 +1,29 @@
+// if else statement
+
+const email = prompt('Enter your email');
+const password = prompt('Enter your password');
+
+if (email) {
+    alert('Hurray! You are logged');
+} else {
+   alert('Try again');
+}
+
+// switch statements
+
+const gmail = prompt('Enter your gmail');
+const gmailpassword = prompt('Enter your gmailpassword');
+
+switch(gmail) {
+    case 'A':
+        alert('Hurray! You are logged')
+      break;
+    case 'B':
+        alert('Try again')
+      break;
+    case 'C':
+        alert('Invalid')
+  }
+
+
+
